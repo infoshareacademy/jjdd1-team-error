2536ed770c08d199b0cf0e29577f159daf8fdb44
Iga Falkowska <falkowska.iga@gmail.com>
2017-03-31 16:39:00 +0200

    Structure added

---
+++ main/java/com/infoshareacademy/jjdd1/teamerror/TerminalMenu.java
@@ -0,0 +1,11 @@
+package com.infoshareacademy.jjdd1.teamerror;
+
+/**
+ * Created by igafalkowska on 31.03.17.
+ */
+public class TerminalMenu {
+    public static void main(String[] args) {
+        System.out.println("hello");
+
+    }
+}
