cf8f29fc3aabf37a19e65d133309ee0577021653
Sebastian Los <los.gdansk@gmail.com>
2017-04-03 15:42:00 +0200

    Method unzipFile implemented. Some comments added.

--- src/main/java/com/infoshareacademy/jjdd1/teamerror/FileReader.java
+++ src/main/java/com/infoshareacademy/jjdd1/teamerror/FileReader.java
@@ -1,5 +1,8 @@
 package com.infoshareacademy.jjdd1.teamerror;
 
+import net.lingala.zip4j.core.ZipFile;
+import net.lingala.zip4j.exception.ZipException;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -36,11 +39,15 @@ public class FileReader {
 
     // divide string into useful parts (words, prices)
     public static String[][] fileFilter(String content) {
-        // split content
+        // split content into single lines
         String[] lines = content.split("\n");
+        // single elements of given line
         String[] parts;
+        // all single elements
         String[][] result = new String[lines.length][];
+
         for (int i = 0; i < lines.length; i++) {
+            // split single lines into single elements
             parts = lines[i].split(",");
             result[i] = new String[parts.length];
             for (int j = 0; j < parts.length; j++)
@@ -50,14 +57,13 @@ public class FileReader {
     }
 
       // unzip the file from and to given location
-//    public static void unzipFile (String source, String destination) {
-//        try {
-//            ZipFile file = new ZipFile(source);
-//            file.extractAll(destination);
-//        } catch (ZipException e) {
-//            System.out.println(e);
-//        } catch (IOException e) {
-//            System.out.println(e);
-//        }
-//    }
+    public static void unzipFile (String source, String destination) {
+        try {
+            ZipFile file = new ZipFile(source);
+            // extract file
+            file.extractAll(destination);
+        } catch (ZipException e) {
+            System.out.println(e);
+        }
+    }
 }
