3a71a523c586662edb22f0b9dbcae8bbd32b4520
samulilaine-zamojski <s.lainezamojski@gmail.com>
2017-04-04 16:53:23 +0200

    added fullCost

---
+++ src/main/java/com/infoshareacademy/jjdd1/teamerror/FullCost.java
@@ -0,0 +1,21 @@
+package com.infoshareacademy.jjdd1.teamerror;
+
+/**
+ * Created by samulilaine-zamojski on 04.04.17.
+ */
+public class FullCost {
+
+
+    int distance = 0;
+    int fuelUsage = 0;
+    int fuelPrice = 0;
+    int currencyRate = 0;
+
+
+
+    int result = distance * fuelUsage * fuelPrice;
+
+
+
+}
+
