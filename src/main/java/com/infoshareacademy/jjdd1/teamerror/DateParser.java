5a51d14252e80d68a63c3fed805d243fb79a1b7a
Kris <krystian.skrzyszewski@gmail.com>
2017-04-06 20:29:10 +0200

    Aesthetic changes + modified DateParser

--- src/main/java/com/infoshareacademy/jjdd1/teamerror/DateParser.java
+++ src/main/java/com/infoshareacademy/jjdd1/teamerror/DateParser.java
@@ -23,9 +23,34 @@ public class DateParser {
             return LocalDate.parse(dateFragment[0], DateTimeFormatter.ofPattern("yyyyMMdd"));
         }
         else{
-            LocalDate date = LocalDate.parse(dateFragment[1].substring(0,3), DateTimeFormatter.ofPattern("MMM"));
-            String month = date.toString();
-            return LocalDate.parse(dateFragment[0]+dateFragment[1]+"01", DateTimeFormatter.ofPattern("yyyyMMdd"));
+            String month ="";
+            switch(dateFragment[1]){
+                case "January": month="01";
+                    break;
+                case "February": month="02";
+                    break;
+                case "March": month="03";
+                    break;
+                case "April": month="04";
+                    break;
+                case "May": month="05";
+                    break;
+                case "June": month="06";
+                    break;
+                case "July": month="07";
+                    break;
+                case "August": month="08";
+                    break;
+                case "September": month="09";
+                    break;
+                case "October": month="10";
+                    break;
+                case "November": month="11";
+                    break;
+                case "December": month="12";
+                    break;
+            }
+            return LocalDate.parse(dateFragment[0]+month+"01", DateTimeFormatter.ofPattern("yyyyMMdd"));
         }
     }
 }
