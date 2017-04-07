4494e9460cdd1d1af415d145c5f0c751e0d2782c
Iga Falkowska <falkowska.iga@gmail.com>
2017-04-07 17:03:40 +0200

    TerminalMenu - add exception

--- src/main/java/com/infoshareacademy/jjdd1/teamerror/TerminalMenu.java
+++ src/main/java/com/infoshareacademy/jjdd1/teamerror/TerminalMenu.java
@@ -3,45 +3,61 @@ package com.infoshareacademy.jjdd1.teamerror;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import static com.infoshareacademy.jjdd1.teamerror.FileReader.fileFilter;
+import static com.infoshareacademy.jjdd1.teamerror.FileReader.loadContent;
+import static com.infoshareacademy.jjdd1.teamerror.FileReader.unzipFile;
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Scanner;
+
+import static com.infoshareacademy.jjdd1.teamerror.FileReader.*;
+import static java.time.temporal.ChronoUnit.DAYS;
+
 /**
  * Created by igafalkowska on 31.03.17.
  */
 public class TerminalMenu {
     public static void main(String[] args) {
-        System.out.println("Menu");
-        System.out.println("------------------");
-        System.out.println("Options:");
-        System.out.println("1 - Option 1");
-        System.out.println("2 - Option 2");
-        System.out.println("3 - Option 3");
-        System.out.println("4 - Option 4");
-        System.out.println("------------------");
-        System.out.println("Select option: ");
+        Scanner input = new Scanner(System.in);
+           String selection = input.nextLine();
 
 
-        try {
-            Scanner input = new Scanner(System.in);
-            int selection = input.nextInt();
 
-            switch (selection) {
-                case 1:
-                    System.out.println("Option 1 selected");
-                    break;
-                case 2:
-                    System.out.println("Option 2 selected");
-                    break;
-                case 3:
-                    System.out.println("Option 3 selected");
-                    break;
-                case 4:
-                    System.out.println("Option 4 selected");
-                    break;
-                default:
-                    System.out.println("Selected invalid number");
-            }
-        } catch (InputMismatchException e) {
-            System.out.println("You didn't select a number");
-        }
+
+//        System.out.println("Menu");
+//        System.out.println("------------------");
+//        System.out.println("Options:");
+//        System.out.println("1 - Option 1");
+//        System.out.println("2 - Option 2");
+//        System.out.println("3 - Option 3");
+//        System.out.println("4 - Option 4");
+//        System.out.println("------------------");
+//        System.out.println("Select option: ");
+//
+//
+//        try {
+//            Scanner input = new Scanner(System.in);
+//            int selection = input.nextInt();
+//
+//            switch (selection) {
+//                case 1:
+//                    System.out.println("Option 1 selected");
+//                    break;
+//                case 2:
+//                    System.out.println("Option 2 selected");
+//                    break;
+//                case 3:
+//                    System.out.println("Option 3 selected");
+//                    break;
+//                case 4:
+//                    System.out.println("Option 4 selected");
+//                    break;
+//                default:
+//                    System.out.println("Selected invalid number");
+//            }
+//        } catch (InputMismatchException e) {
+//            System.out.println("You didn't select a number");
+//        }
     }
 }
 
