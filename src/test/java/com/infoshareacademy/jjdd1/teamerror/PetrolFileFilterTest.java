90a227d0c89a6df07e5c6dd729401e1cf828dd76
samulilaine <s.lainezamojski@gmail.com>
2017-04-27 15:35:34 +0200

    CountryStatics class created, tm working now

--- src/test/java/com/infoshareacademy/jjdd1/teamerror/PetrolFileFilterTest.java
+++
@@ -1,7 +0,0 @@
-package com.infoshareacademy.jjdd1.teamerror;
-
-/**
- * Created by samulilaine-zamojski on 20.04.17.
- */
-public class PetrolFileFilterTest {
-}
---
+++ webApp/src/main/java/com/infoshareacademy/jjdd1/teamerror/dataBase/CountryStatistics.java
@@ -0,0 +1,21 @@
+//package com.infoshareacademy.jjdd1.teamerror.dataBase;
+//
+//import java.util.Map;
+//
+///**
+// * Created by samulilaine on 27/04/2017.
+// */
+//public class CountryStatistics {
+//
+//    Map<String, Long> country;
+//
+//    public Map<String, Long> getCountry() {
+//        return country;
+//    }
+//
+//    public void setCountry(String cos) {
+//        if(!country.containsKey(String cos)){}
+//
+//        this.country=country;
+//    }
+//}
