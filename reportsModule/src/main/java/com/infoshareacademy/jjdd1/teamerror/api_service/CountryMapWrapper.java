560cfd2d126d58ed8f31983b0ef2a562517bbcd3
Sebastian Los <los.gdansk@gmail.com>
2017-05-18 16:49:32 +0200

    CountryMap wrapper initialized.

--- reportsModule/src/main/java/com/infoshareacademy/jjdd1/teamerror/api_service/CountryMapWrapper.java
+++ reportsModule/src/main/java/com/infoshareacademy/jjdd1/teamerror/api_service/CountryMapWrapper.java
@@ -1,7 +1,22 @@
 package com.infoshareacademy.jjdd1.teamerror.api_service;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by sebastianlos on 18.05.17.
  */
 public class CountryMapWrapper {
+    Map<String, Integer> countryStatistics = new HashMap<>();
+
+    public CountryMapWrapper() {
+    }
+
+    public Map<String, Integer> getCountryStatistics() {
+        return countryStatistics;
+    }
+
+    public void setCountryStatistics(Map<String, Integer> countryStatistics) {
+        this.countryStatistics = countryStatistics;
+    }
 }
